edbf7e231c70d23d8b36e801c60bc85be168e821
danRobot <danielgallo502@gmail.com>
2019-05-22 20:47:12 -0500

    otro run

--- run_norm.sh
+++ run_norm.sh
@@ -1,14 +1,14 @@
 rm -rvf results*
 rm -rvf weightsKTH/*.h5
 python3 main.py --epocs 100 --dataset 5
-zip -r resultsKTH_relu.zip resultsKTH/
+zip -r resultsKTH_relu_norm.zip resultsKTH/
 rm -rvf resultsKTH/
 rm -rvf weightsKTH/*.h5
 python3 main.py --epocs 100 --dataset 5 --is_relu 0
-zip -r resultsKTH_leaky_relu.zip resultsKTH/
+zip -r resultsKTH_leaky_relu_norm.zip resultsKTH/
 rm -rvf resultsKTH/
 rm -rvf weightsKTH/*.h5
 python3 main.py --epocs 100 --dataset 5 --has 0
-zip -r resultsKTH_no_act.zip resultsKTH/
+zip -r resultsKTH_no_act_norm.zip resultsKTH/
 rm -rvf resultsKTH/
 rm -rvf weightsKTH/*.h5
