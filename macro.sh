914fec6ef0b361bafed30f033ff80d8051919635
danRobot <danielgallo502@gmail.com>
2019-06-03 01:00:30 -0500

    fix pre test script

--- macro.sh
+++ macro.sh
@@ -1,10 +1,15 @@
+git checkout bs32
+sleep 5
 ./run.sh 5 32
-mkdir resultados_31
+mkdir resultados_32
 mv *.zip resultados_32
 
 git add .
 git commit -m 'batch size 32 11 clases'
 
+
+git checkout bs16
+sleep 5
 ./run.sh 5 16
 mkdir resultados_16
 mv *.zip resultados_16
